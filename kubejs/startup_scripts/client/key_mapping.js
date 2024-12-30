70bffc832981878f98ea297936acaca6c0d0ddee
FalAut <2907281799@qq.com>
2024-12-30 20:39:17 +0800

    删除客户端魔改

--- kubejs/startup_scripts/client/key_mapping.js
+++
@@ -1,5 +0,0 @@
-const $KeyMapping = Java.loadClass("net.minecraft.client.KeyMapping");
-
-ForgeModEvents.onEvent("net.minecraftforge.client.event.RegisterKeyMappingsEvent", (event) => {
-    event.register(new $KeyMapping("key.mierno.portable_crafting", 67, "key.categories.inventory"));
-});

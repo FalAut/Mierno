86b4cb9d7dac10079a9d43dd9154ebc5c61a5a5f
FalAut <2907281799@qq.com>
2024-12-30 20:24:12 +0800

    改为使用KubeJS内置事件注册谓词

--- kubejs/startup_scripts/client/item_properties_register.js
+++ kubejs/startup_scripts/client/item_properties_register.js
@@ -1,16 +1,13 @@
-StartupEvents.postInit((event) => {
-    if (!Platform.isClientEnvironment()) return;
-    const $ItemProperties = Java.loadClass("net.minecraft.client.renderer.item.ItemProperties");
-
-    $ItemProperties.register("mierno:sun_crystal", "solar", (stack, world, living, seed) =>
+ItemEvents.modelProperties((event) => {
+    event.register("mierno:sun_crystal", "mierno:solar", (stack, world, living, seed) =>
         stack.nbt && stack.nbt.contains("Solar") ? stack.nbt.getInt("Solar") / 100 : 0
     );
 
-    $ItemProperties.register("mierno:moon_stone", "lunar", (stack, world, living, seed) =>
+    event.register("mierno:moon_stone", "mierno:lunar", (stack, world, living, seed) =>
         stack.nbt && stack.nbt.contains("Lunar") ? stack.nbt.getInt("Lunar") / 100 : 0
     );
 
-    $ItemProperties.register("mierno:unstable_ingot", "stable", (stack, world, living, seed) =>
+    event.register("mierno:unstable_ingot", "mierno:stable", (stack, world, living, seed) =>
         stack.nbt && stack.nbt.contains("Stable") ? stack.nbt.getInt("Stable") / 100 : 0
     );
 });

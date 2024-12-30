8617ce236ba4e0d8d0056c402b2e18f8a1481f88
FalAut <2907281799@qq.com>
2024-12-30 18:06:41 +0800

    将游戏时间显示处理转移到客户端

--- kubejs/client_scripts/network_client.js
+++ kubejs/client_scripts/network_client.js
@@ -44,6 +44,44 @@ NetworkEvents.dataReceived("display_third_eye", (event) => {
     }
 });
 
+ClientEvents.loggedIn((event) => {
+    event.player.paint({
+        playtime_counter: {
+            type: "text",
+            color: "gray",
+            alignY: "bottom",
+        },
+    });
+});
+
+NetworkEvents.dataReceived("display_playtime", (event) => {
+    const { player, data } = event;
+
+    player.paint({ playtime_counter: { text: convertTime(data.playtime) } });
+});
+
+/**
+ * 转化playTime为天时分秒
+ * @param {number} playTime
+ * @returns
+ */
+function convertTime(playTime) {
+    const totalSeconds = Math.floor(playTime / 20);
+
+    const days = Math.floor(totalSeconds / 86400);
+    const hours = Math.floor((totalSeconds % 86400) / 3600);
+    const minutes = Math.floor((totalSeconds % 3600) / 60);
+    const seconds = totalSeconds % 60;
+
+    let result;
+    if (seconds > 0) result = Text.translate("mierno.time.seconds", seconds.toFixed(0)).getString();
+    if (minutes > 0) result = Text.translate("mierno.time.minutes", minutes.toFixed(0)).getString();
+    if (hours > 0) result = Text.translate("mierno.time.hours", hours.toFixed(0)).getString();
+    if (days > 0) result = Text.translate("mierno.time.days", days.toFixed(0)).getString();
+
+    return result || "";
+}
+
 // ClientEvents.loggedIn((event) => {
 //     let isFlatField = $ClientLevelData.__javaObject__.getDeclaredField("f_104832_");
 
--- kubejs/server_scripts/common/player_event.js
+++ kubejs/server_scripts/common/player_event.js
@@ -2,35 +2,6 @@ PlayerEvents.tick((event) => {
     const { player, level } = event;
     if (player.age % 20) return;
 
-    player.paint({
-        playtime_counter: {
-            type: "text",
-            color: "gray",
-            alignY: "bottom",
-        },
-    });
-
-    player.paint({ playtime_counter: { text: convertTime(player.stats.playTime) } });
-
-    player.setFoodLevel(20);
-    player.setSaturation(20);
-
-    player.inventory.allItems.forEach((item) => {
-        if (item == "mierno:unstable_ingot") {
-            if (!item.nbt) {
-                item.setNbt({ Stable: 100 });
-            }
-
-            let stable = item.nbt.getInt("Stable");
-            item.nbt.putInt("Stable", stable - 10);
-
-            if (stable <= 10) {
-                item.count--;
-                player.block.createExplosion().explosionMode("none").strength(5).explode();
-            }
-        }
-    });
-
     if (level.dimension == "mierno:misty_forest") {
         let foundDreamLamp = player.inventory.allItems.some((item) => processDreamLantern(item, player));
 
@@ -54,11 +25,32 @@ PlayerEvents.tick((event) => {
         player.potionEffects.add("blindness", 60);
     }
 
+    player.inventory.allItems.forEach((item) => {
+        if (item == "mierno:unstable_ingot") {
+            if (!item.nbt) {
+                item.setNbt({ Stable: 100 });
+            }
+
+            let stable = item.nbt.getInt("Stable");
+            item.nbt.putInt("Stable", stable - 10);
+
+            if (stable <= 10) {
+                item.count--;
+                player.block.createExplosion().explosionMode("none").strength(5).explode();
+            }
+        }
+    });
+
+    player.setFoodLevel(20);
+    player.setSaturation(20);
+
     if (hasCurios(player, "botania:third_eye")) {
         player.sendData("display_third_eye", { hasThirdEye: true });
     } else {
         player.sendData("display_third_eye", { hasThirdEye: false });
     }
+
+    player.sendData("display_playtime", { playtime: player.stats.playTime });
 });
 
 EntityEvents.death("player", (event) => {

9051225049014556ace038df169f2cfcf6b0fe5e
FalAut <2907281799@qq.com>
2025-01-03 13:47:52 +0800

    暂存代码

--- kubejs/server_scripts/recipe/mbd2/modular_mana_infusion.js
+++ kubejs/server_scripts/recipe/mbd2/modular_mana_infusion.js
@@ -1,31 +1,6 @@
 ServerEvents.recipes((event) => {
     const { mierno } = event.recipes;
 
-    let manaInfusions = [];
-
-    event.forEachRecipe({ type: "botania:mana_infusion" }, (recipe) => {
-        console.log(recipe.json);
-        let input = recipe.originalRecipeIngredients;
-        let catalyst;
-
-        if (recipe.json.get("catalyst")) {
-            catalyst = recipe.json.get("catalyst").get("block");
-        }
-
-        let output = recipe.originalRecipeResult;
-        let outputCount = recipe.json.get("output").get("count");
-
-        manaInfusions.push({
-            input: input.first.itemIds[0],
-            catalyst: catalyst ? catalyst : "null",
-            output: output.id,
-            outputCount: outputCount ? outputCount : 1,
-            mana: recipe.json.get("mana"),
-        });
-    });
-
-    JsonIO.write("kubejs/mana_infusion.json", { recipes: manaInfusions });
-
     JsonIO.read("kubejs/mana_infusion.json").recipes.forEach((recipeData) => {
         if (recipeData.catalyst != "null") {
             mierno

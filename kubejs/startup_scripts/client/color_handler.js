75271dd527f9f2035c06a08f23a45d15af3384a6
FalAut <2907281799@qq.com>
2025-01-06 21:46:15 +0800

    测

--- kubejs/startup_scripts/client/color_handler.js
+++ kubejs/startup_scripts/client/color_handler.js
@@ -18,32 +18,3 @@ StartupEvents.postInit((event) => {
 
     Client.itemColors.register((stack, index) => (index == 1 ? 4159204 : -1), cobbleGens);
 });
-
-// ForgeEvents.onEvent("net.minecraftforge.client.event.RegisterColorHandlersEvent$Block", (event) => {
-//     event.register(
-//         (state, env, pos, tintIndex) =>
-//             tintIndex == 1 ? (env != null && pos != null ? $BiomeColors.getAverageWaterColor(env, pos) : 4159204) : -1,
-//         [
-//             "mierno:cobble_gen_tier1",
-//             "mierno:cobble_gen_tier2",
-//             "mierno:cobble_gen_tier3",
-//             "mierno:cobble_gen_tier4",
-//             "mierno:cobble_gen_tier5",
-//             "mierno:cobble_gen_tier6",
-//         ]
-//     );
-// });
-
-// ForgeModEvents.onEvent("net.minecraftforge.client.event.RegisterColorHandlersEvent$Item", (event) => {
-//     event.register(
-//         (stack, index) => (index == 1 ? 4159204 : -1),
-//         [
-//             "mierno:cobble_gen_tier1",
-//             "mierno:cobble_gen_tier2",
-//             "mierno:cobble_gen_tier3",
-//             "mierno:cobble_gen_tier4",
-//             "mierno:cobble_gen_tier5",
-//             "mierno:cobble_gen_tier6",
-//         ]
-//     );
-// });

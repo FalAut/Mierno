07ae5e22f42df78dd48e6b1b9672ce6e968ea828
FalAut <2907281799@qq.com>
2025-01-25 11:23:07 +0800

    恢复了数字合成中减法和乘法的配方

--- kubejs/server_scripts/recipe/crafting.js
+++ kubejs/server_scripts/recipe/crafting.js
@@ -1065,27 +1065,27 @@ ServerEvents.recipes((event) => {
                     .keepIngredient("mierno:division_sigil");
             }
 
-            // // 减法
-            // if (i - j >= 0) {
-            //     kubejs
-            //         .shaped(`mierno:${i - j}`, ["ABC"], {
-            //             A: `mierno:${i}`,
-            //             B: Item.of("mierno:subtraction_sigil").enchant("mierno:activate", 1).weakNBT(),
-            //             C: `mierno:${j}`,
-            //         })
-            //         .keepIngredient("mierno:subtraction_sigil");
-            // }
+            // 减法
+            if (i - j >= 0) {
+                kubejs
+                    .shaped(`mierno:${i - j}`, ["ABC"], {
+                        A: `mierno:${i}`,
+                        B: Item.of("mierno:subtraction_sigil").enchant("mierno:activate", 1).weakNBT(),
+                        C: `mierno:${j}`,
+                    })
+                    .keepIngredient("mierno:subtraction_sigil");
+            }
 
-            // // 乘法
-            // if (i * j <= 9) {
-            //     kubejs
-            //         .shaped(`mierno:${i * j}`, ["ABC"], {
-            //             A: `mierno:${i}`,
-            //             B: Item.of("mierno:multiplication_sigil").enchant("mierno:activate", 1).weakNBT(),
-            //             C: `mierno:${j}`,
-            //         })
-            //         .keepIngredient("mierno:multiplication_sigil");
-            // }
+            // 乘法
+            if (i * j <= 9) {
+                kubejs
+                    .shaped(`mierno:${i * j}`, ["ABC"], {
+                        A: `mierno:${i}`,
+                        B: Item.of("mierno:multiplication_sigil").enchant("mierno:activate", 1).weakNBT(),
+                        C: `mierno:${j}`,
+                    })
+                    .keepIngredient("mierno:multiplication_sigil");
+            }
         }
     }
 });

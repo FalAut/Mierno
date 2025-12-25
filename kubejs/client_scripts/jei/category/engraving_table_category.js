a07dcd7ed2d929f3c008862479f22c50bb1deeb5
FalAut <2907281799@qq.com>
2025-12-26 00:30:13 +0800

    为刻印台JEI页面添加配方转移按钮

--- kubejs/client_scripts/jei/category/engraving_table_category.js
+++ kubejs/client_scripts/jei/category/engraving_table_category.js
@@ -1,55 +1,65 @@
 JEIAddedEvents.registerCategories((event) => {
     event.custom('mierno:engraving', (category) => {
-        const {
-            jeiHelpers: { guiHelper },
-        } = category;
+        const { guiHelper } = category.jeiHelpers;
 
         category
             .title(Text.translate('mierno.engraving_table'))
-            .background(guiHelper.createDrawable('mierno:textures/block/engraving_table_top.png', 0, 0, 120, 105))
+            .background(guiHelper.createDrawable('mierno:textures/gui/engraving.png', 0, 0, 180, 130))
             .setWidth(180)
             .setHeight(130)
             .icon(guiHelper.createDrawableItemStack('mierno:engraving_table'))
             .handleLookup((layOut, recipe, focuses) => {
-                layOut.addSlot('INPUT', 45, 40).addItemStack(recipe.data.input0);
+                layOut.addSlot('INPUT', 50, 60).addItemStack(recipe.data.input0);
 
-                layOut.addSlot('INPUT', 45, 5).addItemStack(recipe.data.input1);
-                layOut.addSlot('INPUT', 70, 15).addItemStack(recipe.data.input2);
-                layOut.addSlot('INPUT', 80, 40).addItemStack(recipe.data.input3);
-                layOut.addSlot('INPUT', 70, 65).addItemStack(recipe.data.input4);
-                layOut.addSlot('INPUT', 45, 75).addItemStack(recipe.data.input5);
-                layOut.addSlot('INPUT', 20, 65).addItemStack(recipe.data.input6);
-                layOut.addSlot('INPUT', 10, 40).addItemStack(recipe.data.input7);
-                layOut.addSlot('INPUT', 20, 15).addItemStack(recipe.data.input8);
+                layOut.addSlot('INPUT', 50, 21).addItemStack(recipe.data.input1);
+                layOut.addSlot('INPUT', 74, 35).addItemStack(recipe.data.input2);
+                layOut.addSlot('INPUT', 87, 60).addItemStack(recipe.data.input3);
+                layOut.addSlot('INPUT', 74, 85).addItemStack(recipe.data.input4);
+                layOut.addSlot('INPUT', 50, 103).addItemStack(recipe.data.input5);
+                layOut.addSlot('INPUT', 26, 85).addItemStack(recipe.data.input6);
+                layOut.addSlot('INPUT', 13, 60).addItemStack(recipe.data.input7);
+                layOut.addSlot('INPUT', 26, 35).addItemStack(recipe.data.input8);
 
-                layOut.addSlot('OUTPUT', 112, 42).addItemStack(recipe.data.output);
+                layOut.addSlot('OUTPUT', 139, 60).addItemStack(recipe.data.output);
 
                 layOut.moveRecipeTransferButton(180, 130);
             })
             .setDrawHandler((recipe, recipeSlotsView, guiGraphics, mouseX, mouseY) => {
-                let overlay = guiHelper.createDrawable(
-                    'mierno:textures/gui/aura_grinder_progress_filled.png',
-                    0,
-                    0,
-                    64,
-                    44
-                );
-                overlay.draw(guiGraphics, 24, 0);
+                let auraText = Text.translate('recipe.capability.natures_aura.aura', recipe.data.aura);
+                guiGraphics.drawWordWrap(Client.font, auraText, 100, 38, 100, 0);
             });
     });
 });
 
 JEIAddedEvents.registerRecipes((event) => {
-    event.custom('mierno:engraving').add({
-        output: 'mierno:moon_stone',
-        input0: 'mierno:colorless_gem',
-        input1: 'forbidden_arcanus:mundabitur_dust',
-        input2: 'naturesaura:calling_spirit',
-        input3: 'naturesaura:sky_ingot',
-        input4: 'naturesaura:sky_ingot',
-        input5: 'forbidden_arcanus:aureal_bottle',
-        input6: 'naturesaura:sky_ingot',
-        input7: 'naturesaura:sky_ingot',
-        input8: 'naturesaura:calling_spirit',
+    let recipes = Client.getConnection().getRecipeManager().getAllRecipesFor('mierno:engraving_table');
+
+    recipes.forEach((/**@type {Internal.MBDRecipe}*/ recipe) => {
+        let auraValue = 0;
+        let inputs = [];
+
+        recipe.inputs.entrySet().forEach((entry) => {
+            entry.value.forEach((content) => {
+                /**@type {Internal.Ingredient} */
+                let i = content.getContent();
+
+                if (i.TYPE == 'mbd2:sized') {
+                    inputs.push(i.first.id);
+                } else if (i.TYPE == 'int') {
+                    auraValue = i;
+                }
+            });
+        });
+
+        let recipeData = {
+            aura: auraValue,
+            output: recipe.outputs.entrySet().toArray()[0].value[0].content.first.id,
+        };
+
+        for (let i = 0; i < 9; i++) {
+            recipeData[`input${i}`] = inputs[i];
+        }
+
+        event.custom('mierno:engraving').add(recipeData);
     });
 });

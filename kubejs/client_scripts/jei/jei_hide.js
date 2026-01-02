a2cf656e0cd83b7173f4a3aa8103be73f5299727
FalAut <2907281799@qq.com>
2026-01-02 20:19:18 +0800

    隐藏了最低级的植物机械，现在你可以从基础级别做起

--- kubejs/client_scripts/jei/jei_hide.js
+++ kubejs/client_scripts/jei/jei_hide.js
@@ -13,6 +13,12 @@ JEIEvents.hideItems((event) => {
     event.hide('ad_astra:oil_bucket');
     event.hide('botanicalextramachinery:catalyst_stone_infinity');
     event.hide('botanicalextramachinery:catalyst_wood_infinity');
+    event.hide('botanicalmachinery:alfheim_market');
+    event.hide('botanicalmachinery:industrial_agglomeration_factory');
+    event.hide('botanicalmachinery:mechanical_apothecary');
+    event.hide('botanicalmachinery:mechanical_daisy');
+    event.hide('botanicalmachinery:mechanical_runic_altar');
+    event.hide('botanicalmachinery:mechanical_mana_pool');
 });
 
 JEIEvents.hideFluids((event) => {
